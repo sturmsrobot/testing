2d65d01db6b71d1eb208c6bce0c15a8157418e69
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-03-04 14:42:37 +0100

    HA, Aufgabe 2.2

---
+++ unit-testing/minMax.test.js
@@ -0,0 +1,25 @@
+const minMax = require("./minMax");
+
+test("Teste die min-Funktion mit zwei positiven Zahlen", () => {
+  expect(minMax.min(3, 5)).toEqual(3);
+});
+
+test("Teste die min-Funktion mit einer negativen und einer positiven Zahl", () => {
+  expect(minMax.min(-3, 5)).toEqual(-3);
+});
+
+test("Teste die min-Funktion mit einer Zahl und 0", () => {
+  expect(minMax.min(0, 5)).toEqual(0);
+});
+
+test("Teste die max-Funktion mit zwei positiven Zahlen", () => {
+  expect(minMax.max(3, 5)).toEqual(5);
+});
+
+test("Teste die max-Funktion mit einer negativen und einer positiven Zahl", () => {
+  expect(minMax.max(-3, 5)).toEqual(5);
+});
+
+test("Teste die max-Funktion mit einer Zahl und 0", () => {
+  expect(minMax.max(0, 5)).toEqual(5);
+});
