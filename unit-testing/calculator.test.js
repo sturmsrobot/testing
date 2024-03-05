636ce5f849b95f76d3ed7ee3c38fdbef2beb59be
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-03-05 13:51:36 +0100

    HA_Di, Aufgabe 1

--- unit-testing/calculator.test.js
+++ unit-testing/calculator.test.js
@@ -1,25 +1,31 @@
 const calculator = require("./calculator");
 
-test("Teste die Subtraktion von zwei positiven Zahlen", () => {
-  expect(calculator.substract(5, 3)).toEqual(2);
-});
+describe("Calculator Functions", () => {
+  describe("Substraction", () => {
+    test("Teste die Subtraktion von zwei positiven Zahlen", () => {
+      expect(calculator.substract(5, 3)).toEqual(2);
+    });
 
-test("Teste die Subtraktion von einer negativen und einer positiven Zahl", () => {
-  expect(calculator.substract(-5, 3)).toEqual(-8);
-});
+    test("Teste die Subtraktion von einer negativen und einer positiven Zahl", () => {
+      expect(calculator.substract(-5, 3)).toEqual(-8);
+    });
 
-test("Teste die Subtraktion von einer Zahl und 0", () => {
-  expect(calculator.substract(5, 0)).toEqual(5);
-});
+    test("Teste die Subtraktion von einer Zahl und 0", () => {
+      expect(calculator.substract(5, 0)).toEqual(5);
+    });
+  });
 
-test("Teste die Multiplikation von zwei positiven Zahlen", () => {
-  expect(calculator.multiply(2, 3)).toEqual(6);
-});
+  describe("Multiplication", () => {
+    test("Teste die Multiplikation von zwei positiven Zahlen", () => {
+      expect(calculator.multiply(2, 3)).toEqual(6);
+    });
 
-test("Teste die Multiplikation von einer negativen und einer positiven Zahl", () => {
-  expect(calculator.multiply(-2, 3)).toEqual(-6);
-});
+    test("Teste die Multiplikation von einer negativen und einer positiven Zahl", () => {
+      expect(calculator.multiply(-2, 3)).toEqual(-6);
+    });
 
-test("Teste die Multiplikation von einer Zahl und 0", () => {
-  expect(calculator.multiply(5, 0)).toEqual(0);
+    test("Teste die Multiplikation von einer Zahl und 0", () => {
+      expect(calculator.multiply(5, 0)).toEqual(0);
+    });
+  });
 });
