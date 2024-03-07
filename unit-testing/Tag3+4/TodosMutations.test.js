37a18386f48d92b206a81643de4f7f6562917d6d
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-03-07 13:53:07 +0100

    Dateien geladen

---
+++ unit-testing/Tag3+4/TodosMutations.test.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const TodosMutations = require("./TodosMutations");
+
+// Axios .get Methode soll die weitere Funktion mockResolvedValue erhalten
+jest.mock("axios");
+
+const returnedTodo1 = {
+  id: 6,
+  userId: 1,
+  task: "Trinken",
+  isDone: true,
+  dueDate: "2024-11-10T00:00:00.000Z",
+  createdAt: "2024-02-28T08:17:33.000Z",
+  updatedAt: "2024-02-28T13:10:19.000Z",
+};
+
+describe("Testing POST Functions of Todo Route", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Teste POST Create Funktion", async () => {
+    const mockData = {
+      data: { todo: returnedTodo1 },
+    };
+    axios.post.mockResolvedValue(mockData);
+    const myDate = new Date();
+    const result = await TodosMutations.createTodo(1, "Essen", false, myDate);
+
+    expect(result).toEqual(mockData.data.todo);
+    expect(axios.post).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledWith(
+      "http://localhost:5050/v1/todos/create",
+      { newDueDate: myDate, newIsDone: false, newTask: "Essen", newUserId: 1 }
+    );
+  });
+});
